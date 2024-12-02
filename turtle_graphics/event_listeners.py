e92a1c86ae0558ed3145dacc8267f8b468e77a67
Manoj Nagula <manojnagula44@gmail.com>
2024-12-03 01:05:22 +0530

    snake is moving

---
+++ turtle_graphics/event_listeners.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+
+def move_forward():
+    timmy.forward()
+
+def move_backward():
+    timmy.backward()
+
+def move_counter_clockwise():
+    timmy.c
+
+def move_forward():
+    timmy.forward()
+
+screen = Screen()
+screen.exitonclick()
