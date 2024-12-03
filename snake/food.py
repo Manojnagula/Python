d3222fd69d702724c8e01ee06d36527faa409bdf
Manoj Nagula <manojnagula44@gmail.com>
2024-12-03 11:21:50 +0530

    able to detect collission with food

--- snake/food.py
+++ snake/food.py
@@ -8,7 +8,9 @@ class Food(Turtle):
         self.speed('fastest')
         self.color('blue')
         self.shapesize(stretch_len=0.5,stretch_wid=0.5)
+        self.refresh()
+       
+    def refresh(self):
         x_random = random.randint(-300,300)
         y_random = random.randint(-300,300)
         self.goto(x_random ,y_random)
-
