71c4972a3dce21cc626b23b15c8891ac96865c28
Manoj Nagula <manojnagula44@gmail.com>
2025-01-23 01:51:13 +0530

    completed game over sequence

--- turtle-cross/main.py
+++ turtle-cross/main.py
@@ -2,12 +2,18 @@ from player import Player
 import time
 from turtle import Screen
 from car_manager import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+
 car_manager = CarManager()
 player = Player()
+score_board = Scoreboard()
+
+
 screen.listen()
 screen.onkey(player.go_up,'Up')
 game_is_on = True
@@ -21,10 +27,14 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player)<20:
             game_is_on=False
+            score_board.game_over()
         
     # Check whether the player reached finish line
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        score_board.increase_level()
+
+    
     
 screen.exitonclick()
