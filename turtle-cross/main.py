636585a117c63041e336e79a2be37f699f75bf49
Manoj Nagula <manojnagula44@gmail.com>
2025-01-23 01:06:38 +0530

    able to create cars and move them across the road

---
+++ turtle-cross/main.py
@@ -0,0 +1,21 @@
+from player import Player
+import time
+from turtle import Screen
+from car_manager import CarManager
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+car_manager = CarManager()
+player = Player()
+screen.listen()
+screen.onkey(player.go_up,'Up')
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    # detect collission with car
+    
